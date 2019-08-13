ab22111c96eb3c0241d4888e85aa6f36ca8083b7
Todd Leonhardt <todd.leonhardt@gmail.com>
2019-08-12 21:25:20 -0400

    Add Python 3.8 to setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Software Development :: Libraries :: Python Modules
 """.splitlines())))
