17ada41ce9e97aa92edda724c97177e504588157
Eric Lin <anselor@gmail.com>
2021-01-12 12:36:18 -0500

    Added PEP-561 tag file. Updated to use git tags for wheel versions

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ Setuptools setup file, used to install or test 'tableformatter'
 """
 from setuptools import setup
 
-VERSION = '0.1.5'
 DESCRIPTION = "python-tableformatter - Tabular data formatter allowing printing from both arbitrary tuples of strings or object inspection"
 LONG_DESCRIPTION = """tableformatter is a tabular data formatter allowing printing from both arbitrary tuples of strings or object inspection.
 It converts your data into a string form suitable for pretty-printing as a table.  The goal is to make it quick and easy
@@ -43,9 +42,13 @@ EXTRAS_REQUIRE = {
     ]
 }
 
+PACKAGE_DATA = {
+    '': ['py.typed']
+}
+
 setup(
     name="tableformatter",
-    version=VERSION,
+    use_scm_version=True,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     classifiers=CLASSIFIERS,
@@ -59,4 +62,5 @@ setup(
     python_requires='>=3.4',
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
+    package_data=PACKAGE_DATA,
 )
